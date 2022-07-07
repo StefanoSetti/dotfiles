c446c23d3da8fea0f71fcc8055124bf05bd168c3
Stefano Setti <s.setti@affidaty.io>
2022-07-07 21:59:16 +0200

    Added video series configuration.

--- nvim/lua/vnmrd/colorscheme.lua
+++ nvim/lua/vnmrd/colorscheme.lua
@@ -1,1 +1,9 @@
--- TODO: Add video logic.
+-- This file contains the colorscheme.
+
+local colorscheme = "default"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
