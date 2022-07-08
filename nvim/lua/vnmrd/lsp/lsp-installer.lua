71bb938c3502885c4e35c6f83bf62aaf67c7bb4c
Stefano Setti <s.setti@affidaty.io>
2022-07-08 23:54:38 +0200

    Added firt implementation of LSP.

---
+++ nvim/lua/vnmrd/lsp/lsp-installer.lua
@@ -0,0 +1,36 @@
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+	return
+end
+
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+	local opts = {
+		on_attach = require("vnmrd.lsp.handlers").on_attach,
+		capabilities = require("vnmrd.lsp.handlers").capabilities,
+	}
+
+	-- The following commands expands the `opts` table depending on the type of file in use.
+
+	 if server.name == "jsonls" then
+	 	local jsonls_opts = require("vnmrd.lsp.settings.jsonls")
+	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+	 end
+
+	 if server.name == "sumneko_lua" then
+	 	local sumneko_opts = require("vnmrd.lsp.settings.sumneko_lua")
+	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	 end
+
+	 -- TODO
+	 if server.name == "rust" then
+	 	local rust_opts = require("vnmrd.lsp.settings.rust")
+	 	opts = vim.tbl_deep_extend("force", rust_opts, opts)
+	 end
+
+	-- This setup() function is exactly the same as lspconfig's setup function.
+	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+	server:setup(opts)
+end)
+
--- nvim/lua/vnmrd/plugins.lua
+++ nvim/lua/vnmrd/plugins.lua
@@ -54,8 +54,15 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
-    -- TODO: add cargo snippet plugin
+  use "hrsh7th/cmp-nvim-lsp" -- cmp integration for LSPs
+  use "hrsh7th/cmp-nvim-lua" -- TODO: ???
 
+  -- TODO: add cargo snippet plugin
+
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  
   -- snippets
   use "L3MON4D3/LuaSnip" -- snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use (rust too)
