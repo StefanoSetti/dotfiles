49c937205cef07ba99a423801950a763be5a01a3
Stefano Setti <s.setti@affidaty.io>
2022-07-11 16:45:05 +0200

    Added dashboard and custom header, to resize.

--- nvim/lua/vnmrd/plugins.lua
+++ nvim/lua/vnmrd/plugins.lua
@@ -74,6 +74,9 @@ return packer.startup(function(use)
   }
   use "p00f/nvim-ts-rainbow"
 
+  -- Dashboard
+  use "glepnir/dashboard-nvim"
+
   -- snippets
   use "L3MON4D3/LuaSnip" -- snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use (rust too)
