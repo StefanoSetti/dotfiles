db44d0bfb933a763e9986b78d757f6eb703bc403
Stefano Setti <s.setti@affidaty.io>
2022-07-13 11:26:05 +0200

    Minor changes.

--- nvim/lua/vnmrd/plugins.lua
+++ nvim/lua/vnmrd/plugins.lua
@@ -62,12 +62,12 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
- 
+
   -- Telescope
   use "nvim-telescope/telescope.nvim"
-  use "nvim-telescope/telescope-media-files.nvim"  
+  use "nvim-telescope/telescope-media-files.nvim"
   use "BurntSushi/ripgrep"
- 
+
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
